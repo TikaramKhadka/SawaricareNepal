6cdf97633997c34742f1c7ebe45b543bf8f7854f
Tikaramkhadka <tikaram.itexperts@gmail.com>
2024-08-22 22:12:14 +0545

    commit product form

--- react/sawaricare/sawaricarenepal/src/app/(authenticate)/Brand/page.js
+++ react/sawaricare/sawaricarenepal/src/app/(authenticate)/Brand/page.js
@@ -1,9 +1,42 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Input, Textarea } from '@nextui-org/react'
+import React from 'react'
 const Brand = () => {
     return (
-      <div>
-        <h1 className="text-3xl font-bold">Brand Management</h1>
-        <p>This is the brand management section.</p>
-      </div>
+      <div className="w-full h-full p-4">
+      <Card>
+        <CardHeader className="p-4 border-b">
+          <div className="flex justify-between items-center w-full">
+            <h3 className="text-lg font-extrabold">Brand Details</h3>
+          </div>
+        </CardHeader>
+
+        <CardBody className="gap-4 p-4">
+          <div>
+            <form className="w-full md:w-1/2">
+              <Input
+                type="text"
+                label="Brand Name"               
+              />
+              <Textarea
+                type="text"
+                label="Brand Description"               
+                className="mt-4"
+              />
+               <Input
+                type="file"
+                label="Choose File"                
+              />
+            </form>
+          </div>
+        </CardBody>
+        
+        <CardFooter className="p-4">
+          <Button className="bg-slate-800 text-white uppercase">
+            Add Brand
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
     );
   };
   export default Brand;
--- react/sawaricare/sawaricarenepal/src/app/(authenticate)/product/page.js
+++ react/sawaricare/sawaricarenepal/src/app/(authenticate)/product/page.js
@@ -1,32 +1,68 @@
-import { Card, CardBody, CardFooter, CardHeader, Input } from '@nextui-org/react'
-import React from 'react'
-const product = () => {
+import { Button, Card, CardBody, CardFooter, CardHeader, Input, Textarea } from '@nextui-org/react';
+import React from 'react';
+
+const Product = () => {
   return (
-    <div className='w-full h-full'>
-    <Card>
-    <CardHeader className='p-0 m-0 border'>
-    <div >
-        <h3 className='p-3 font-extrabold'>Product Details</h3>
+    <div className="w-full h-full p-4">
+      <Card>
+        <CardHeader className="p-4 border-b">
+          <div className="flex justify-between items-center w-full">
+            <h3 className="text-lg font-extrabold">Product Details</h3>
+          </div>
+        </CardHeader>
+        <CardBody className="gap-4 p-4">
+          <form className="w-full">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <Input
+                type="text"
+                label="Product Name"               
+              />
+              <Input
+                type="text"
+                label="Category Name"               
+              />
+                <Input
+                type="text"
+                label="Sub Category Name"               
+              />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+              <Input
+                type="number"
+                label="Unit Price"               
+              /> 
+              <Input
+                type="number"
+                label="Discount Price"               
+              />
+                <Input
+                type="number"
+                label="Quantity"               
+              />
+            </div>
+            
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+              <Textarea
+              type="text"
+              label="Product Description" 
+              />       
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+             <Input
+                type="file"
+                label="Choose File"                
+              />
+              </div>
+          </form>
+        </CardBody>
+        <CardFooter className="p-4">
+          <Button className="bg-slate-800 text-white uppercase">
+            Add Product
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
-    </CardHeader>
-  <CardBody className='gap-3'>
-  <Input  
-      type="text"
-      label="Product Name" 
-      labelPlacement="outside"         
-    /> 
-   
-</CardBody>
-<CardFooter>      
-<Button className='bg-slate-800 mt-2 text-white font-bold uppercase'>
-    Add Product
-</Button>
- </CardFooter>
-</Card>
-</div>
-  )
+  );
 }
-export default product
 
-
-
+export default Product;
