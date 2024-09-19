500de9eb8f983d96e60480913d0400683f9bcc95
Tikaramkhadka <tikaram.itexperts@gmail.com>
2024-09-19 22:30:09 +0545

    added env in user

--- nodejs/src/route/user.js
+++ nodejs/src/route/user.js
@@ -4,7 +4,7 @@ const UserRoute = Router();
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
 
-mongoose.connect('mongodb://localhost:27017/mydb', 
+mongoose.connect(process.env.DB_CONNECTION, 
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
